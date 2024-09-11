658b8c2789fe876e2a42e1e06cd8794b8821f586
Muhammadzidan21 <muhammadzidan2003@mail.ugm.ac.id>
2024-09-11 20:41:55 +0700

    no.2

---
+++ no2.py
@@ -0,0 +1,33 @@
+Nama  = input("masukan nama :")
+Prodi = input("masukan prodi :")
+IPK   = float(input("masukan nilai IPK :"))
+
+if IPK >3.90 or IPK==4:
+          print("Predikat Summa Cumlaude")
+          print("Selamat usaha kamu tidak menghianati hasil")
+          print("Tetaplah menjadi orang yang rendah hati dan terus menginspirasi")
+elif IPK > 3.71 or IPK == 3.90:
+          print("predikat Magna Cumlaude")
+          print("Jangan pernah puas akan hasil yang telah dicapai")
+          print("Teruslah bermimpi setinggi langit, jika kamu jatuh, kamu akan jatuh diantara bintang-bintang")
+elif IPK > 3.51 or IPK == 3.70 :
+          print("Predikat  Cumlaude")
+          print("Banggalah terhadap dirimu dengan hasil yang telah diraih")
+          print("Terus mengimprove dirimu supaya menjadi lebih baik lagi")
+elif IPK > 3.01 or IPK == 3.50 :
+          print("Predikat Sangat Memuaskan")
+          print("Harus tetap bersyukur dengan hasil yang telah dicapai")
+          print("Jangan berhenti disini, teruslah perbaiki dirimu !!")
+elif IPK > 2.76 or IPK == 3.00 :
+          print("Predikat Memuaskan")
+          print("Jangan putus asa, masih ada kesempatan teruslah berusaha dan berdoa")
+          print("Ingat hasil tidak akan menghianati hasil")
+elif IPK < 2.76 :
+          print ("Tanpa Predikat")
+          print("Jangan pernah putus asa IPK bukanlah penentu akhir dari kemampuan atau masa depanmu")
+          print("Fokus pada Perbaikan Bukan Kesalahan, jangan terlalu keras pada diri sendiri ")
+print("SELAMAT JADI ALUMNI GADJAH MADA")
+      
+  
+  
+
